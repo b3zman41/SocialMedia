6c1fd334d10a43034a3ad611683a536f8618499a
Terence Bezman <Terence@terences-mbp.home>
2014-04-09 21:20:23 -0400

    Initial Commit

---
+++ SocialMedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocialMedia
+//
+//  Created by Terence Bezman on 4/9/14.
+//  Copyright (c) 2014 Terence Bezman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialMedia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SocialMedia
+//
+//  Created by Terence Bezman on 4/9/14.
+//  Copyright (c) 2014 Terence Bezman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
